a014f2258700671034292333be8191186267fb47
Olukanni Oluwafunmilola deborah <funmilolaabiola002@gmail.com>
2021-07-12 13:51:17 +0100

    Files created and updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import "../styles.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Edit to see some magic happen!</h2>
+<Header />
+<AddContact />
+<ContactList />
     </div>
   );
 }
