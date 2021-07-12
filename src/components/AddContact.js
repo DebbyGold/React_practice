f6aaf6e8a48bf7e8b2f5cd71483ea1b262fdf70a
Olukanni Oluwafunmilola deborah <funmilolaabiola002@gmail.com>
2021-07-12 16:58:12 +0100

    Updated

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 export default function AddContact(){
-  return(
-<div>
+return(
+<div className="ui main">
+<h3>Add Contacts</h3> 
+<form className="ui form">
+<div className="ui field">
+<label>Name</label> 
+<input type="text" name="name" placeholder="Enter Name here..." />
+</div> 
+<div className="ui field">
+<label>Email</label>
+<input type="email" name="email" />
 </div>
-  )
+</form>
+</div> 
+)
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "../components/Global.css";
 
-const style={
-textAlign:"center",
-}
-
 export default function Header(){
 return(
-<div className="ui fixed menu">
-<h2 style={style}>Contact Manager</h2>
+<div className="ui center">
+<h2>Contact Manager</h2>
+<hr />
 </div>
 )
 }
