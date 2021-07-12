ed14b0915f319451497a7632a37990905e63c0c8
Olukanni Oluwafunmilola deborah <funmilolaabiola002@gmail.com>
2021-07-12 14:35:06 +0100

    Index file updated

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,6 @@ import ContactCard from "./ContactCard";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Edit to see some magic happen!</h2>
 <Header />
 <AddContact />
 <ContactList />
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function Header(){
 return(
-<div>
+<div className="ui fixed menu">
+<div className="ui container center">
+<h2>Contact Manager</h2>
+</div>
 </div>
 )
 }
