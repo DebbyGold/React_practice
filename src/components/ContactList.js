40b7e22cb92dbe41e2c440dafff44fb480a94d97
Olukanni Oluwafunmilola deborah <funmilolaabiola002@gmail.com>
2021-07-15 08:56:53 +0100

    Updated

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,9 +2,19 @@ import React from "react";
 
 export default function ContactList(props){
 
+const renderContactLists=props.contacts.map((contact) => {
 return(
-<div className="ui celled list">
-<h4>Contact Lists</h4>
+<div className="item">
+<div className="content">
+<div className="header">{contact.name}</div>
+<div>{contact.email}</div> 
+<i className="trash alternative outline icon"></i> 
 </div>
-)
+</div>
+);
+});
+return <div className="ui celled list">
+{renderContactLists}
+</div>
+
 }
