9db7e040fe53c4ebd93870d34fb8ca613433380d
Olukanni Oluwafunmilola deborah <funmilolaabiola002@gmail.com>
2021-07-16 14:21:23 +0100

    Updated

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-export default function ContactCard(){
+export default function ContactCard(props){
+const [id, Name, Email]=props.contacts.map((contact)=>{
+})
   return(
-<div>
+<div className="item">
+<div className="content">
+<div className="header">{Name}</div> 
+<div>{Email}</div> 
+<i className="trash alternate outline icon"></i> 
+</div> 
 </div>
   )
 }
