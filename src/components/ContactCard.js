ad01896190ac29e8682b61674551474e792db8df
Olukanni Oluwafunmilola deborah <funmilolaabiola002@gmail.com>
2021-07-17 18:03:09 +0100

    Updated

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -6,13 +6,15 @@ const {Id, Name, Email}=props.contact;
 
   return(
 <div className="item">
-<div className="content">
+<div style={{display:"flex", marginBottom:"15px"}} className="content">
 <img className="ui avatar image" src={User} alt="user" />
+<div style={{display:"block",marginLeft:"7px"}}>
 <div className="header">{Name}</div> 
 <div>{Email}</div>
 </div> 
+</div>
 <div>
-<i style={{float: right}} className="trash alternate outline icon"></i>
+<i style={{float: "right", marginTop:"-45px"}} className="trash alternate outline icon"></i>
 </div>
 </div>
   )
