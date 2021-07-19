ae379b9b1ce467be1e1dd29ee420aa020e75b3cb
Olukanni Oluwafunmilola deborah <funmilolaabiola002@gmail.com>
2021-07-19 08:58:07 +0100

    Updated

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -9,17 +9,25 @@ export default function AddContact(){
 name:"",
 email:"",
   }
+const add=(e)=>{
+e.preventDefault();
+if(this.state.name==="" && this.state.name===""){
+alert("All fields are mandatory");
+return
+}
+console.log(this.state);
+}
 return(
 <div style={style} className="ui main">
 <h3 style={{textAlign:"center"}}>Add Contacts</h3> 
-<form className="ui form">
+<form className="ui form" onSubmit={this.add}>
 <div className="ui field">
 <label>Name</label> 
-<input type="text" name="name" placeholder="Name" value={this.state.name} onChange={(e) => this.setState({name:e.target.value})} />
+<input type="text" name="name" placeholder="Name" value={this.name} onChange={(e)=>this.setState({name:e.target.name})}/>
 </div> 
 <div className="ui field">
 <label>Email</label>
-<input type="email" name="email" placeholder="Email" value={this.state.email} onChange={(e) => this.setState({email:e.target.value})}/>
+<input type="email" name="email" placeholder="Email" value={this.email} onChange={(e) => this.setState({email:e.target.value})} />
 <button className="ui button blue" style={{marginTop:"15px"}}>Add</button>
 </div>
 </form>
